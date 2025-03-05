20a4b8f4f0143ddc3627565656ff7c80f2708e28
MarioSijabat <149126439+MarioSijabat@users.noreply.github.com>
2025-03-05 08:43:33 +0700

    main commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html', // Include the main HTML file
+    './src/**/*.{html,js}', // Include HTML and JS files in the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
